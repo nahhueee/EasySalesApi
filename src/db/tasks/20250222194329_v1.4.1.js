666be80eece02973a5a8edcfba534e1a9012e58f
nahhueee <nahu852na@gmail.com>
2025-05-17 13:13:22 -0300

    Configuraciones de build

--- src/db/tasks/20250222194329_v1.4.1.js
+++
@@ -1,8 +0,0 @@
-exports.up = function(knex) {
-    return knex('parametros').insert({ clave: 'actualizacion', valor: 'false' });
-};
-  
-exports.down = function(knex) {
-    return knex('parametros').where('actualizacion', 'false').del();
-};
-  
